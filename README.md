0115644778e3cf664fe9b521b268a23c70698eb3
Muntaser Qutub <muntaser.qutub@relativity.com>
2019-09-13 11:40:20 -0500

    Adding ConvertTo-AzFromAzureRm6 script and updating README

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # AzConversion
 A utility to convert from AzureRM 6 to Az PowerShell
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+PowerShell
+
+## Deployment
+
+The script to convert from AzureRM 6 to Az is easy to execute. It uses mappings from Azure's Github site and converts an entire directory and everything under it to Az PowerShell using those mappings.
+
+```
+S:\SourceCode\AzConversion\src\ConvertTo-AzFromAzureRm6.ps1 -RootDirectory S:\SourceCode\myrepo
+S:\SourceCode\AzConversion\src\ConvertTo-AzFromAzureRm6.ps1 -RootDirectory S:\SourceCode\myrepo -CustomMappings @{ "mapthis" = "toThis" }
+```
+
+## Built With
+
+* [PowerShell 5.1](https://docs.microsoft.com/en-us/powershell/scripting/overview?view=powershell-5.1) - PowerShell 5.1
+
+## Authors
+
+* **Muntaser Qutub** - *Initial work* - [PurpleBooth](https://github.com/muntaserq)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ src/ConvertTo-AzFromAzureRm6.ps1
@@ -0,0 +1,80 @@
+<#
+.SYNOPSIS
+  A PowerShell script to convert a directory and all sub directories from AzureRM 6 to Az PowerShell.
+
+.DESCRIPTION
+  A PowerShell script to convert a directory and all sub directories from AzureRM 6 to Az PowerShell.
+
+.PARAMETER RootDirectory
+  The root directory to start the conversion from.
+
+.PARAMETER CustomMappings
+  Optional: Any custom mappings in code that you want to be converted. This parameter type is a Hashtable.
+
+.EXAMPLE
+  .\ConvertTo-AzFromAzureRm6.ps1 -RootDirectory 'S:\SourceCode\myrepo'
+  .\ConvertTo-AzFromAzureRm6.ps1 -RootDirectory 'S:\SourceCode\myrepo' -CustomMappings @{ "testAzureRm6" = "testAz" }
+#>
+[CmdletBinding()]
+param(
+  [Parameter(Mandatory = $true)]
+  [ValidateScript( {
+      if ( -Not ($_ | Test-Path) )
+      {
+        throw "File or folder does not exist"
+      }
+      return $true
+    })]
+  [String] $RootDirectory,
+
+  [Parameter(Mandatory = $false)]
+  [System.Collections.Hashtable] $CustomMappings = @{ }
+)
+
+Set-Location -Path $RootDirectory
+
+$items = Get-ChildItem "$RootDirectory\*" -Exclude @('*.exe', '*.json') -Recurse -Depth 100 | Where-Object { !$_.PSisContainer }
+$azureMappings = ((Invoke-WebRequest -Uri https://raw.githubusercontent.com/Azure/azure-powershell/master/src/Accounts/Accounts/AzureRmAlias/Mappings.json -UseBasicParsing).Content | ConvertFrom-Json)
+
+$customMappingsAsObject = [PSCustomObject]$CustomMappings
+
+foreach ($item in $items)
+{
+  $fullPathFileName = $item.FullName
+  $fileContents = (Get-Content $fullPathFileName -Raw)
+
+  Write-Host "Processing file $fullPathFileName"
+
+  $fileTouched = $false
+
+  ($azureMappings | Get-Member -MemberType NoteProperty) | ForEach-Object {
+    $azureMappings.$($_.Name) | ForEach-Object {
+      foreach ($Mapping in ($_ | Get-Member -MemberType NoteProperty))
+      {
+        if (-not $fileTouched)
+        {
+          $fileTouched = $fileContents -match $_.$($Mapping.Name)
+        }
+
+        $fileContents = $fileContents -replace $_.$($Mapping.Name), $Mapping.Name
+      }
+    }
+  }
+
+  ($customMappingsAsObject | Get-Member -MemberType NoteProperty) | ForEach-Object {
+    $name = [regex]::Escape($($_.Name))
+    $valueToReplace = $customMappingsAsObject.$($_.Name)
+    if (-not $fileTouched)
+    {
+      $fileTouched = $fileContents -match $name
+    }
+
+    $fileContents = $fileContents -replace $name, $valueToReplace
+  }
+
+  if ($fileTouched)
+  {
+    $fileContents | Set-Content $fullPathFileName -NoNewline
+    Write-Host "Saved file $fullPathFileName"
+  }
+}
